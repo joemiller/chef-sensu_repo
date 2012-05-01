dfb39ac9dd4f2094bf319b471aea475ae1a5a2bd
joe miller <joeym@joeym.net>
2012-05-01 10:34:56 -0700

    added deps to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,15 @@
 maintainer        "Joe Miller"
 maintainer_email  "joeym@joeym.net"
 license           "Apache 2.0"
-description       "configures sensu-build box"
+description       "configures sensu apt and yum repo management functions"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 
 # available @ http://community.opscode.com/cookbooks/apt
 depends "apt"
+
+# available @ http://community.opscode.com/cookbooks/git
+depends "git"
 
 %w{ ubuntu debian }.each do |os|
   supports os
