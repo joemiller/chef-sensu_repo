04ea0edb1d2cfc54f088d7290866ab6ebf402465
joe miller <joeym@joeym.net>
2012-05-02 16:11:30 -0700

    fixed path to web files in sync_to_s3.rb

--- files/default/scripts/sync_to_s3.rb
+++ files/default/scripts/sync_to_s3.rb
@@ -2,4 +2,4 @@
 
 require File.join(File.dirname(__FILE__), 'settings')
 
-system "s3cmd --acl-public --delete-removed sync #{REPO_BASE}/html s3://#{S3_BUCKET}/"
+system "s3cmd --acl-public --delete-removed sync #{REPO_BASE}/html/ s3://#{S3_BUCKET}/"
