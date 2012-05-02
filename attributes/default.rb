afdc317bf582f9ea82c88cc79fc7b1f121322948
joe miller <joeym@joeym.net>
2012-05-02 12:50:29 -0700

    updated name/email on gpg key

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,9 +6,9 @@ default.sensu_repo.apt.origin = 'Sensu'
 default.sensu_repo.apt.label = 'Sensu'
 default.sensu_repo.apt.distro = "apt/sensu"
 
-default.sensu_repo.gpg.name.real "Sensu Package Maintainer"
-default.sensu_repo.gpg.name.comment "package signing key"
-default.sensu_repo.gpg.name.email "testingonly@joemiller.me"
+default.sensu_repo.gpg.name.real "Sonian Inc"
+default.sensu_repo.gpg.name.comment "Sensu signing key"
+default.sensu_repo.gpg.name.email "chefs@sonian.net"
 default.sensu_repo.gpg.expire.date "0"
 default.sensu_repo.gpg.batch_config "/tmp/gpg_batch_config"
 
