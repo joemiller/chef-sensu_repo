17b0b41f4c7d8440ae12b05ab6c3f8002817fd4e
joe miller <joeym@joeym.net>
2012-05-02 20:11:27 -0700

    need to install s3cmd from source

--- recipes/s3cmd.rb
+++ recipes/s3cmd.rb
@@ -1,3 +1,22 @@
-package "s3cmd" do
-  action :install
+# package "s3cmd" do
+#   action :install
+# end
+
+s3cmd_source_url = "https://github.com/s3tools/s3cmd/tarball/master"
+
+remote_file "#{Chef::Config['file_cache_path']}/s3cmd.tar.gz" do
+  source s3cmd_source_url
+  mode 0644
+  notifies :run, "execute[install-s3cmd]", :immediately
+end
+
+bash "install-s3cmd" do
+  user "root"
+  cwd Chef::Config['file_cache_path']
+  code <<-EOH
+    tar -zxf s3cmd.tar.gz
+    mkdir s3cmd
+    tar zxf s3cmd.tar.gz --strip-components 1 -C s3cmd
+  EOH
+  action :nothing
 end
