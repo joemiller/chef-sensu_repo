14a1a6235d6337aae1f97d3c9a226715dd0fbfb6
joe miller <joeym@joeym.net>
2012-05-02 19:38:31 -0700

    added --follow-symlinks to s3cmd

--- files/default/scripts/sync_to_s3.rb
+++ files/default/scripts/sync_to_s3.rb
@@ -2,4 +2,5 @@
 
 require File.join(File.dirname(__FILE__), 'settings')
 
-system "s3cmd --acl-public --delete-removed sync #{REPO_BASE}/html/ s3://#{S3_BUCKET}/"
+system "s3cmd --acl-public --delete-removed --follow-symlinks \
+  sync #{REPO_BASE}/html/ s3://#{S3_BUCKET}/"
